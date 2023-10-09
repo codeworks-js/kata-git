0bbffd2634192e29f58ef5307ab3383edc4c1a52
Edwin Oursel <edwinoursel@edwins-mbp.home>
2023-10-09 15:29:53 +0200

    [004] code review

--- src/Lift.ts
+++ src/Lift.ts
@@ -1,8 +1,8 @@
 import { Button } from "./Button";
-import {User} from "./User";
+import { User } from "./User";
 
 export class Lift {
-    MAX_WEIGHT = 800
+    private static readonly MAX_WEIGHT = 800
     constructor(private buttons: Button[], private users: User[]) {}
 
 }
